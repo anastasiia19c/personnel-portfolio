d745f00d8d40deac9732e01eb16efb330bb1f1b7
anastasiia19c <156657920+anastasiia19c@users.noreply.github.com>
2024-12-27 21:13:29 +0100

    feat: add the filter for projects

--- src/components/projects/ProjectsGrid.vue
+++ src/components/projects/ProjectsGrid.vue
@@ -18,10 +18,41 @@
       </p>
     </div>
 
+    <!-- Filter buttons -->
+    <div class="text-center mb-6">
+      <button
+        @click="filter = 'all'"
+        :class="{'bg-secondary-light dark:bg-secondary-dark text-primary-dark dark:text-primary-light': filter === 'all', 
+                'dark:text-primary-light': filter !== 'all',
+                'text-white': filter === 'all'}"
+        class="px-4 py-2 mr-4 rounded"
+      >
+        Tous
+      </button>
+      <button
+        @click="filter = 'solo'"
+        :class="{'bg-secondary-light dark:bg-secondary-dark text-primary-dark dark:text-primary-light': filter === 'solo', 
+                'dark:text-primary-light': filter !== 'solo',
+                'text-white': filter === 'solo'}"
+        class="px-4 py-2 mr-4 rounded"
+      >
+        Projets Solo
+      </button>
+      <button
+        @click="filter = 'team'"
+        :class="{'bg-secondary-light dark:bg-secondary-dark text-primary-dark dark:text-primary-light': filter === 'team',
+                'dark:text-primary-light': filter !== 'team',
+                'text-white': filter === 'team'}"
+        class="px-4 py-2 rounded"
+      >
+        Projets d'Équipe
+      </button>
+    </div>
+
     <!-- Projects grid -->
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10">
       <ProjectCell
-        v-for="project in projectSummaries"
+        v-for="project in filteredProjects" 
         :key="project.id"
         :project="project"
       />
@@ -35,17 +66,33 @@
   import ProjectCell from "./ProjectCell.vue";
   import projectSummaries from "../../data/projectSummaries";
 
-  export default {
-    name: "ProjectGrid",
-    components: { ProjectCell },
-    data: () => {
-      return {
-        projectSummaries
-      };
-    },
-    mounted() {
-      feather.replace();
-    },
-  };
+export default {
+  name: "ProjectGrid",
+  components: { ProjectCell },
+  data() {
+    return {
+      filter: 'all', 
+      projectSummaries: projectSummaries, 
+    };
+  },
+  mounted() {
+    feather.replace();
+  },
+  computed: {
+    filteredProjects() {
+      console.log("Filtre appliqué: ", this.filter);
+      if (this.filter === 'all') {
+        return this.projectSummaries; 
+      }
+      if (this.filter === 'solo') {
+        return this.projectSummaries.filter(project => project.isSolo); 
+      }
+      if (this.filter === 'team') {
+        return this.projectSummaries.filter(project => project.isTeam); 
+      }
+      return this.projectSummaries; 
+    }
+  }
+};
 </script>
 
--- src/data/projectSummaries.js
+++ src/data/projectSummaries.js
@@ -5,6 +5,8 @@ const projectSummaries = [
     id: 0,
     title: "Cavalons",
     category: "Full-stack",
+    isSolo: false,
+    isTeam: true,
     date: "Novembre 2024 - Décembre 2024",
     displayImg: require("@/assets/images/projet7-1.png"),
     galleryImg: [
@@ -17,6 +19,8 @@ const projectSummaries = [
     title: "Portfolio",
     category: "Front-end",
     date: "Octobre 2024",
+    isSolo: true,
+    isTeam: false,
     link: "https://github.com/anastasiia19c/personnel-portfolio",
     displayImg: require("@/assets/images/projet6-2.png"),
     galleryImg: [
@@ -27,6 +31,8 @@ const projectSummaries = [
     id: 2,
     title: "Fil Rouge 4.0.2",
     category: "Full-stack",
+    isSolo: true,
+    isTeam: false,
     date: "Septembre 2024 - Novembre 2024",
     link: "https://github.com/anastasiia19c/fil-Rouge2",
     displayImg: require("@/assets/images/projet1-4.png"),
@@ -40,6 +46,8 @@ const projectSummaries = [
     id: 3,
     title: "Objectif-Admission",
     category: "Full-stack",
+    isSolo: false,
+    isTeam: true,
     date: "Avril 2024 - Juin 2024",
     displayImg: require("@/assets/images/projet2-4.png"),
     galleryImg: [
@@ -52,6 +60,8 @@ const projectSummaries = [
     id: 4,
     title: "NetiOne",
     category: "Full-stack",
+    isSolo: false,
+    isTeam: true,
     date: "Janvier 2024 - Février 2024",
     displayImg: require("@/assets/images/projet3-2.jpg")
   },
@@ -59,6 +69,8 @@ const projectSummaries = [
     id: 5,
     title: "New Skills",
     category: "Full-stack",
+    isSolo: false,
+    isTeam: true,
     date: "Novembre 2023 - Décembre 2023",
     displayImg: require("@/assets/images/projet4-4.png"),
     galleryImg: [
@@ -71,6 +83,8 @@ const projectSummaries = [
     id: 6,
     title: "Fil Rouge 4.0.1",
     category: "Full-stack",
+    isSolo: true,
+    isTeam: false,
     date: "Octobre 2023 - Novembre 2023",
     displayImg: require("@/assets/images/projet5-1.png"),
     galleryImg: [
